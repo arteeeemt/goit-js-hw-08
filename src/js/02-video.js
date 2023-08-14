ad8a7da1af7fabb449598dd09d2b73a06015a5ce
arteeeemt <kesha514@ukr.net>
2023-08-14 12:10:58 +0300

    asd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,35 +7,33 @@ console.log(throttle)
 
 const iframe = document.getElementById('vimeo-player');
 
-// Создаем экземпляр плеера
+
 const player = new Player(iframe);
 
-// Функция для сохранения времени воспроизведения в локальное хранилище
 const saveCurrentTime = (time) => {
     localStorage.setItem('videoplayer-current-time', JSON.stringify(time));
 };
 
-// Функция для загрузки времени воспроизведения из локального хранилища
+
 const loadCurrentTime = () => {
     const savedTime = localStorage.getItem('videoplayer-current-time');
     return savedTime ? JSON.parse(savedTime) : 0;
 };
 
-// Функция для обновления времени воспроизведения
+
 const updateCurrentTime = throttle((currentTime) => {
     saveCurrentTime(currentTime);
-}, 1000); // Ограничиваем вызов функции не чаще чем раз в секунду
+}, 1000); 
 
-// Инициализируем плеер и восстанавливаем время воспроизведения
+
 player.on('timeupdate', (event) => {
     const currentTime = event.seconds;
     updateCurrentTime(currentTime);
 });
 
-// Загружаем и восстанавливаем время воспроизведения при загрузке страницы
+
 player.ready().then(() => {
     const currentTime = loadCurrentTime();
     player.setCurrentTime(currentTime);
 });
-
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,39 +1,40 @@
-// import throttle from 'lodash.throttle';
-// let throttle = require('lodash.throttle');
-// console.log('hh')
-
-// const form = document.querySelector(".feedback-form")
-// const email = document.querySelector("input")
-// const message = document.querySelector("textarea")
-// // const submit = document.querySelector(".submit-btn")
-
-// conso
-// form.addEventListener('input', checkForm)
+import throttle from 'lodash.throttle';
+let throttle = require('lodash.throttle');
 
 
-// window.addEventListener('DOMContentLoaded', function () {
-//     const saveValueOfInput = this.localStorage.getItem("email");
-//     const saveValueOfMessage = this.localStorage.getItem("message")
-//     if (saveValueOfInput !== null) {
-//         email.value = JSON.parse(saveValueOfInput)
-//     }
-//     if (saveValueOfMessage !== null) {
-//         message.value = JSON.parse(saveValueOfMessage)
-//     }
-// })
-console.log('true')
-// function checkForm(evt) {
-//     evt.preventDefault()
+const form = document.querySelector(".feedback-form")
+const email = document.querySelector("input")
+const message = document.querySelector("textarea")
+// const submit = document.querySelector(".submit-btn")
+
+
+form.addEventListener('input', checkForm)
+
+
+window.addEventListener('DOMContentLoaded', function () {
+    const saveValueOfInput = this.localStorage.getItem("email");
+    const saveValueOfMessage = this.localStorage.getItem("message")
+    if (saveValueOfInput !== null) {
+        email.value = JSON.parse(saveValueOfInput)
+    }
+    if (saveValueOfMessage !== null) {
+        message.value = JSON.parse(saveValueOfMessage)
+        
+    }
     
-//     const valueToSaveInput = email.value
+})
+
+function checkForm(evt) {
+    evt.preventDefault()
     
-//     const valueToSaveMessage = message.value
-//     localStorage.setItem("email", JSON.stringify(valueToSaveInput))
-//     localStorage.setItem("message", JSON.stringify(valueToSaveMessage))
-// }
+    const valueToSaveInput = email.value
+    
+    const valueToSaveMessage = message.value
+    localStorage.setItem("email", JSON.stringify(valueToSaveInput))
+    localStorage.setItem("message", JSON.stringify(valueToSaveMessage))
+    
+}
 
 // form.addEventListener("click", clickHandler)
-    
-
     
 
