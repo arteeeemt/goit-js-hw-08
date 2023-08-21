95795bf4e8b580ec1521b1c55514206d8ee047fd
arteeeemt <kesha514@ukr.net>
2023-08-21 12:07:01 +0300

    add

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,40 +1,29 @@
-// import throttle from 'lodash.throttle';
-// const throttle = require('lodash.throttle')
+import throttle from 'lodash.throttle';
+const throttle = require('lodash.throttle')
 
-// const STORAGE_KEY = 'feedback-form-state';
-// const STORAGE_DATA = JSON.parse(localStorage.getItem(STORAGE_KEY))
+const STORAGE_KEY = 'feedback-form-state';
+const STORAGE_DATA = JSON.parse(localStorage.getItem(STORAGE_KEY))
 
-// const form = document.querySelector("form");
-// const input = document.querySelector("input");
-// const message = document.querySelector("textarea");
-
-
-// form.addEventListener('input',throttle(dataFormSave,500))
-
-// if(STORAGE_DATA){
-//     form.elements.email.value = STORAGE_DATA.value
-//     form.elements.message.value = STORAGE_DATA.message
-// }
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const message = document.querySelector("textarea");
 
 
-// function dataFormSave(){
-//     const data = {
-//         email:form.elements.email.value,
-//         message:form.elements.message.value
-//     }
-//     localStorage.setItem(STORAGE_KEY,JSON.stringify(data))
-// }
+form.addEventListener('input',throttle(dataFormSave,500))
 
-// form.addEventListener('submit',function(evt){
-//     if (email.value === '' || message.value === ''){
-//         return alert('Заполните все поля!')
-//     }
-//     evt.preventDefault()
-//     console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)))
-//     form.reset()
-//     localStorage.removeItem(STORAGE_KEY)
-// })
+if(STORAGE_DATA){
+    form.elements.email.value = STORAGE_DATA.value
+    form.elements.message.value = STORAGE_DATA.message
+}
 
+
+function dataFormSave(){
+    const data = {
+        email:form.elements.email.value,
+        message:form.elements.message.value
+    }
+    localStorage.setItem(STORAGE_KEY,JSON.stringify(data))
+}
 
 document.addEventListener("DOMContentLoaded", function() {
     const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
@@ -48,18 +37,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
     form.addEventListener('submit', function (evt) {
         evt.preventDefault();
-        if (form.elements.email.value === '' && form.elements.message.value === '') {
-            return alert('Заповніть усі поля!');
-        }
+        
         const formData = {
             email: form.elements.email.value,
             message: form.elements.message.value
         };
-        console.log(formData);  // Выводим данные в консоль
+        console.log(formData);  
         form.reset();
     });
     
-    // После загрузки страницы, принудительно установите значения полей формы из localStorage
+    
     if (savedData) {
         form.elements.email.value = savedData.email || '';
         form.elements.message.value = savedData.message || '';
